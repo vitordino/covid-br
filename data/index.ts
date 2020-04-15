f3caec707521da44e9f55782ecb4ebc9312c3a50
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-15 01:11:25 -0300

    :truck: move getStatesData to own file

--- data/index.ts
+++ data/index.ts
@@ -1,36 +1,7 @@
-const https = require('https')
-const fs = require('fs')
-const { parse } = require('@fast-csv/parse')
+const getStatesData = require('./getStatesData')
 
-const output: Array<Object> = []
-
-const url =
-	'https://cdn.jsdelivr.net/gh/wcota/covid19br@master/cases-brazil-total.csv'
-
-const handleError = (err: string) => {
-	throw new Error(err)
+const main = () => {
+	getStatesData()
 }
 
-const handleData = (data: object) => output.push(data)
-
-const handeWrite = (err: Error | String) => {
-	if (err) return console.error(err)
-	return console.log('file was saved')
-}
-
-const handleEnd = async (rowCount: number) => {
-	console.log(`Parsed ${rowCount} rows`)
-	fs.writeFile(
-		`${__dirname}/../public/data/total.json`,
-		JSON.stringify(output),
-		handeWrite,
-	)
-}
-
-https.get(url, (res: any) => {
-	res
-		.pipe(parse({ headers: true }))
-		.on('error', handleError)
-		.on('data', handleData)
-		.on('end', handleEnd)
-})
+module.exports = main
