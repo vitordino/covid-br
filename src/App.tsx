b366ff64f0f356b9508d7facb3ab5a7c31ab57d6
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-15 02:26:07 -0300

    :sparkles: render data json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import data from './data/total.json'
 
-const App = () => <pre>app here</pre>
+const App = () => <pre>{JSON.stringify({ data }, null, 2)}</pre>
 
 export default App
