dd0a2bad1853458ce5540c5bb2e9fad76a616a66
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-15 23:20:57 -0300

    :sparkles: add slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { groupBy } from 'ramda'
 
 import data from './data/total.json'
@@ -34,8 +34,22 @@ const groupByDate = groupBy(getDates)
 const pickLatest = (acc: number, curr: string) => Math.max(dateToInt(curr), acc)
 const latestDate = IntToDate(dates.reduce(pickLatest, 0))
 const byDate = groupByDate(cleanData)
-const latestData = byDate[latestDate]
 
-const App = () => <pre>{JSON.stringify(latestData, null, 2)}</pre>
+const App = () => {
+	const [index, setIndex] = useState(dates.length - 1)
+	const currentDate = dates[index]
+	return (
+		<>
+			<input
+				type='range'
+				min='0'
+				max={dates.length - 1}
+				value={index}
+				onChange={({ target }) => setIndex(parseInt(target.value))}
+			/>
+			<pre>{JSON.stringify(byDate[currentDate], null, 2)}</pre>
+		</>
+	)
+}
 
 export default App
