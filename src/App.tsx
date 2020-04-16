d2e4cd7e0e3690c1559f8e9d3466e24fa3dce68b
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-15 21:17:12 -0300

    :sparkle: temporary massage data on frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
 import React from 'react'
+import { groupBy } from 'ramda'
+
 import data from './data/total.json'
 
-const App = () => <pre>{JSON.stringify({ data }, null, 2)}</pre>
+type StateEntry = {
+	date: string
+	state: string
+	deaths: string
+	newDeaths: string
+	newCases: string
+	totalCases: string
+}
+
+const cleanData = data.map(
+	({ date, state, deaths, newDeaths, newCases, totalCases }: StateEntry) => ({
+		date,
+		state,
+		deaths,
+		newDeaths,
+		newCases,
+		totalCases,
+	}),
+)
+
+const removeSlashes = (s: string) => s.replace(/-/g, '')
+const toInt = (s: string) => Number.parseInt(s)
+const dateToInt = (s: string) => toInt(removeSlashes(s))
+const toDate = (s: string) => s.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3')
+const IntToDate = (i: number) => toDate(String(i))
+const getDates = ({ date }: StateEntry) => date
+const dates = cleanData.map(getDates)
+const groupByDate = groupBy(getDates)
+const pickLatest = (acc: number, curr: string) => Math.max(dateToInt(curr), acc)
+const latestDate = IntToDate(dates.reduce(pickLatest, 0))
+const byDate = groupByDate(cleanData)
+const latestData = byDate[latestDate]
+
+const App = () => <pre>{JSON.stringify(latestData, null, 2)}</pre>
 
 export default App
