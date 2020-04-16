fbbb82a2f09ab8386222f52d9cbc81adc95215c7
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-16 18:00:36 -0300

    :lipstick: grid + trend on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React, { useState, useMemo } from 'react'
 
+import BaseTrend from './components/Trend'
 import StatesTable from './components/StatesTable'
+import Container from './components/Container'
+import Grid from './components/Grid'
 import Layout from './components/Layout'
 
 import type { StateEntry, StateEntries } from './components/StatesTable'
 
 import data from './data/states.json'
+
+const Trend = (props: any) => (
+	<BaseTrend
+		smooth
+		autoDraw
+		autoDrawEasing='ease-out'
+		gradient={['#00c6ff', '#F0F', '#FF0']}
+		radius={5}
+		strokeWidth={2}
+		strokeLinecap={'round'}
+		{...props}
+	/>
+)
 
 export type Main = {
 	[key: string]: StateEntry[]
@@ -20,6 +36,12 @@ const removeTotal = (lines: StateEntries) =>
 const findTotal = (lines: StateEntries) =>
 	lines.filter(({ st }) => st === 'TOTAL')[0]
 
+const identity = (x: any) => x
+const x = Object.values(main)
+	.flatMap(identity)
+	.filter(({ st }) => st === 'TOTAL')
+	.map((x) => parseInt(x.tc))
+
 const App = () => {
 	const [index, setIndex] = useState(dates.length - 1)
 	const data: StateEntries = useMemo(() => removeTotal(main[dates[index]]), [
@@ -31,15 +53,26 @@ const App = () => {
 
 	return (
 		<Layout>
-			<input
-				type='range'
-				min='0'
-				max={dates.length - 1}
-				value={index}
-				onChange={({ target }) => setIndex(parseInt(target.value))}
-			/>
-			<pre>{dates[index]}</pre>
-			<StatesTable data={data} total={total} />
+			<Container>
+				<Grid.Row>
+					<Grid.Column xs={8} md={4}>
+						<Trend data={x} />
+					</Grid.Column>
+				</Grid.Row>
+				<input
+					type='range'
+					min='0'
+					max={dates.length - 1}
+					value={index}
+					onChange={({ target }) => setIndex(parseInt(target.value))}
+				/>
+				<pre>{dates[index]}</pre>
+				<Grid.Row>
+					<Grid.Column xs={16} lg={8}>
+						<StatesTable data={data} total={total} />
+					</Grid.Column>
+				</Grid.Row>
+			</Container>
 		</Layout>
 	)
 }
