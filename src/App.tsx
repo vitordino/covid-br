18eb8378ca74c3ffb5c756eb435d3ef4ffef794d
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-18 21:38:30 -0300

    :sparkles: render brazilian states map

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import React, { useState, useMemo } from 'react'
+import {
+	ComposableMap,
+	Geographies,
+	Geography,
+	ZoomableGroup,
+} from 'react-simple-maps'
 
 import BaseTrend from './components/Trend'
 import StatesTable from './components/StatesTable'
@@ -9,6 +15,8 @@ import Layout from './components/Layout'
 import type { StateEntry, StateEntries } from './components/StatesTable'
 
 import data from './data/states.json'
+
+const geography = '/topo/states.json'
 
 const Trend = (props: any) => (
 	<BaseTrend
@@ -42,6 +50,37 @@ const x = Object.values(main)
 	.filter(({ st }) => st === 'TOTAL')
 	.map((x) => parseInt(x.tc))
 
+type Coords = [number, number]
+
+type MapConstraints = {
+	zoom: number
+	lat: number
+	long: number
+	coords: Coords
+}
+
+const mapConstraints: MapConstraints = {
+	zoom: 1,
+	lat: -54,
+	long: -14,
+	coords: [-54, -14],
+}
+
+const mapStyle = {
+	default: {
+		fill: '#D6D6DA',
+		outline: 'none',
+	},
+	hover: {
+		fill: '#E42',
+		outline: 'none',
+	},
+	pressed: {
+		fill: '#E42',
+		outline: 'none',
+	},
+}
+
 const App = () => {
 	const [index, setIndex] = useState(dates.length - 1)
 	const data: StateEntries = useMemo(() => removeTotal(main[dates[index]]), [
@@ -54,22 +93,40 @@ const App = () => {
 	return (
 		<Layout>
 			<Container>
-				<Grid.Row>
-					<Grid.Column xs={8} md={4}>
-						<Trend data={x} />
-					</Grid.Column>
-				</Grid.Row>
-				<input
-					type='range'
-					min='0'
-					max={dates.length - 1}
-					value={index}
-					onChange={({ target }) => setIndex(parseInt(target.value))}
-				/>
-				<pre>{dates[index]}</pre>
-				<Grid.Row>
+				<Grid.Row vertical-gutter>
 					<Grid.Column xs={16} lg={8}>
+						<input
+							type='range'
+							min='0'
+							max={dates.length - 1}
+							value={index}
+							onChange={({ target }) => setIndex(parseInt(target.value))}
+						/>
+						<pre>{dates[index]}</pre>
+						<Trend data={x} />
 						<StatesTable data={data} total={total} />
+					</Grid.Column>
+					<Grid.Column xs={16} lg={8}>
+						<ComposableMap
+							data-tip=''
+							projectionConfig={{ scale: 550, center: [-54, -13] }}
+							height={440}
+							style={{ height: 'auto', width: '100%' }}
+							width={400}
+							projection='geoMercator'
+						>
+							<Geographies geography={geography}>
+								{({ geographies }) =>
+									geographies.map((geo) => (
+										<Geography
+											key={geo.rsmKey}
+											geography={geo}
+											style={mapStyle}
+										/>
+									))
+								}
+							</Geographies>
+						</ComposableMap>
 					</Grid.Column>
 				</Grid.Row>
 			</Container>
