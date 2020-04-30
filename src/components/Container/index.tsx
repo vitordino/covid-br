28c1fa24bb708084bf6d695da3871abf85da019b
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-30 03:29:54 -0300

    :lipstick: container: smaller horizontal margins on mobile

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-	padding-left: 1rem;
-	padding-right: 1rem;
+	padding-left: 0.5rem;
+	padding-right: 0.5rem;
 	max-width: 80rem;
 	margin: 0 auto;
+	${p => p.theme.above('sm')`
+		padding-left: 1rem;
+		padding-right: 1rem;
+	`}
 	${p => p.theme.above('md')`
 		padding-left: 2rem;
 		padding-right: 2rem;
