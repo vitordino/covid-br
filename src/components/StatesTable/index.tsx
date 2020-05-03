f56fc9354ed90b49087932364d4bbd17f187beca
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-02 21:17:41 -0300

    :lipstick: round relative cells to 2 decimal plates

--- src/components/StatesTable/index.tsx
+++ src/components/StatesTable/index.tsx
@@ -214,7 +214,7 @@ const initialState: InitialTableState = {
 const RelativeRender = ({ x, isNew }: { x: number; isNew?: boolean }) => (
 	<span title={`${isNew ? '+ ' : ''}${x * 10000} a cada 10 mil hab`}>
 		{!!isNew && '+ '}
-		{(x * 10000).toFixed(3)}‱
+		{(x * 10000).toFixed(2)}‱
 	</span>
 )
 
