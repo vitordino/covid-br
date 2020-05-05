dcbde7c1c4386983a0b79b252cffdf347fb78c20
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-05 02:20:50 -0300

    :fire: simpler spacer component

--- src/components/Spacer/index.tsx
+++ src/components/Spacer/index.tsx
@@ -1,28 +1,16 @@
 import styled from 'styled-components'
 import { mapBreakpoints } from 'etymos'
 
-type HorizontalProps = {
-	block: boolean
-	margin: number
+type Props = {
+	[key: string]: number
 }
 
-type VerticalProps = {
-	inline: boolean
-	margin: number
-}
-
-const H = styled.div<HorizontalProps>`
-	display: ${({ block }) => (block ? 'flex' : 'inline-flex')};
-	${mapBreakpoints((value, { margin }) =>
-		margin ? `margin: 0 ${value / 2}rem;` : `padding: 0 ${value / 2}rem;`,
-	)}
+const H = styled.div<Props>`
+	${mapBreakpoints(value => `padding: 0 ${value / 2}rem;`)}
 `
 
-const V = styled.div<VerticalProps>`
-	display: ${({ inline }) => (inline ? 'inline-flex' : 'flex')};
-	${mapBreakpoints((value, { margin }) =>
-		margin ? `margin: ${value / 2}rem 0;` : `padding: ${value / 2}rem 0;`,
-	)}
+const V = styled.div<Props>`
+	${mapBreakpoints(value => `padding: ${value / 2}rem 0;`)}
 `
 
 const Spacer = { V, H }
