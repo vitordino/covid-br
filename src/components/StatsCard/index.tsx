9ecc4d890292928ebb39230abdf8ce6455aad859
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-05 02:09:01 -0300

    :sparkles: render internals (relative + isNew) on StatsCard

--- src/components/StatsCard/index.tsx
+++ src/components/StatsCard/index.tsx
@@ -64,22 +64,112 @@ const dataMappingsBySort: DataMappingsOf<StateEntry> = {
 	},
 }
 
+enum Scopes {
+	confirmed,
+	death,
+	recovered,
+}
+
+enum Kinds {
+	absolute,
+	relative,
+}
+
+type TypeMap = {
+	scope: keyof typeof Scopes
+	isNew: boolean
+	kind: keyof typeof Kinds
+}
+
+type TypeMappings = {
+	[K in keyof StateEntry]?: TypeMap
+}
+
+const typeMapping: TypeMappings = {
+	tc: { scope: 'confirmed', isNew: false, kind: 'absolute' },
+	nc: { scope: 'confirmed', isNew: true, kind: 'absolute' },
+	ptc: { scope: 'confirmed', isNew: false, kind: 'relative' },
+	pnc: { scope: 'confirmed', isNew: true, kind: 'relative' },
+	td: { scope: 'death', isNew: false, kind: 'absolute' },
+	nd: { scope: 'death', isNew: true, kind: 'absolute' },
+	ptd: { scope: 'death', isNew: false, kind: 'relative' },
+	pnd: { scope: 'death', isNew: true, kind: 'relative' },
+	tr: { scope: 'recovered', isNew: false, kind: 'absolute' },
+	nr: { scope: 'recovered', isNew: true, kind: 'absolute' },
+	ptr: { scope: 'recovered', isNew: false, kind: 'relative' },
+	pnr: { scope: 'recovered', isNew: true, kind: 'relative' },
+}
+
+type RenderValueProps = {
+	value: number | ReactNode
+	isNew?: boolean
+	kind?: keyof typeof Kinds
+}
+
+const MULTIPLIER = 10000
+
+const RenderValue = ({ value, isNew, kind }: RenderValueProps) => {
+	if (typeof value !== 'number') return <>{value}</>
+	if (kind === 'relative')
+		return (
+			<>
+				{isNew ? '+ ' : ''}
+				{(value * MULTIPLIER).toFixed(2)}‱
+			</>
+		)
+	return (
+		<>
+			{isNew ? '+ ' : ''}
+			{value}
+		</>
+	)
+}
+
+type RenderProps = {
+	value: number | ReactNode
+	isNew?: boolean
+	kind?: keyof typeof Kinds
+	bold?: boolean
+}
+
+const Render = ({ bold, ...props }: RenderProps) => {
+	if (bold)
+		return (
+			<strong>
+				<RenderValue {...props} />
+			</strong>
+		)
+	return (
+		<span>
+			<RenderValue {...props} />
+		</span>
+	)
+}
+
 const StatsCard = ({ label, hoveredData, ...props }: StatsCardProps) => {
 	const sort = useStore(s => s.sort)
 	const { main, mainAlt, sub, subAlt } = dataMappingsBySort?.[sort] || {}
 
 	return (
 		<Wrapper {...props}>
-			{label && <div>{label}</div>}
+			{main && <div>{typeMapping[main]?.scope}</div>}
 			<div>
-				{!!main && <strong>{hoveredData?.[main]}</strong>}
+				{!!main && (
+					<Render bold value={hoveredData?.[main]} {...typeMapping[main]} />
+				)}
 				{!!main && !!mainAlt && ' '}
-				{!!mainAlt && <span>{hoveredData?.[mainAlt]}</span>}
+				{!!mainAlt && (
+					<Render value={hoveredData?.[mainAlt]} {...typeMapping[mainAlt]} />
+				)}
 			</div>
 			<div>
-				{!!sub && <strong>{hoveredData?.[sub]}</strong>}
+				{!!sub && (
+					<Render bold value={hoveredData?.[sub]} {...typeMapping[sub]} />
+				)}
 				{!!sub && !!subAlt && ' '}
-				{!!subAlt && <span>{hoveredData?.[subAlt]}</span>}
+				{!!subAlt && (
+					<Render value={hoveredData?.[subAlt]} {...typeMapping[subAlt]} />
+				)}
 			</div>
 		</Wrapper>
 	)
