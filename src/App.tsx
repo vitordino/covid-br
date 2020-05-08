5da8d3dc8be5818845e755560442889134a11150
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-08 02:51:22 -0300

    :sparkles: mock router

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,19 @@
-import React, { useMemo, useLayoutEffect } from 'react'
-import styled from 'styled-components'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import useStore from 'store'
-import useRelativeSortSync from 'hooks/useRelativeSortSync'
-import StatesTable from 'components/StatesTable'
-import StatesMap from 'components/StatesMap'
-import RelativeSwitcher from 'components/RelativeSwitcher'
-import Container from 'components/Container'
-import Grid from 'components/Grid'
-import Text from 'components/Text'
+import Home from 'routes/Home'
 import Layout from 'components/Layout'
-import RangeInput from 'components/RangeInput'
-import StatsCard from 'components/StatsCard'
 
-import data from 'data/states.json'
-
-// @ts-ignore
-const main: Main = data.main
-// @ts-ignore
-const totals: Totals = data.totals
-
-const dates: DatesEnum[] = data.dates
-const statesMeta: StatesMeta = data.states
-
-const TitleHeader = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: baseline;
-	padding: 0 0.5rem 1rem;
-`
-
-const options = { day: 'numeric', month: 'numeric', year: 'numeric' }
-
-const dateToString = (d: string, l: string = 'pt') =>
-	new Date(d).toLocaleDateString(l, options)
-
-const App = () => {
-	const hoveredState = useStore(s => s.hoveredState)
-	const relative = useStore(s => s.relative)
-	const [dateIndex, setDateIndex] = useStore(s => [s.dateIndex, s.setDateIndex])
-	const data: StateEntry[] = useMemo(() => main[dates[dateIndex]], [dateIndex])
-	const total: StateEntry = useMemo(() => totals[dates[dateIndex]], [dateIndex])
-
-	const caseProp = relative ? 'ptc' : 'tc'
-	const deathProp = relative ? 'ptd' : 'td'
-	const recoveredProp = relative ? 'ptr' : 'tr'
-
-	// @ts-ignore
-	const hoveredTitle = statesMeta?.[hoveredState]?.n || 'Total'
-	const hoveredData = hoveredState
-		? data?.find(({ st }) => st === hoveredState)
-		: total
-
-	useLayoutEffect(() => {
-		setDateIndex(dates.length - 1)
-	}, [setDateIndex])
-
-	useRelativeSortSync()
-
-	return (
+const App = () => (
+	<Router>
 		<Layout>
-			<Container>
-				<TitleHeader>
-					<Text as='h1' weight={400} xs={3} md={4} lg={5} style={{ flex: 1 }}>
-						Brazil
-					</Text>
-					<Text weight={400} xs={2} md={3}>
-						<select
-							value={dateIndex}
-							onChange={({ target }) => setDateIndex(parseInt(target.value))}
-						>
-							{dates.map((x, i) => (
-								<option key={x} value={i}>
-									{dateToString(x)}
-								</option>
-							))}
-						</select>
-					</Text>
-				</TitleHeader>
-				<Grid.Row>
-					<Grid.Column xs={16} lg={10}>
-						<RelativeSwitcher />
-						<StatesTable data={data} total={total} statesMeta={statesMeta} />
-					</Grid.Column>
-					<Grid.Column xs={16} lg={6}>
-						<RelativeSwitcher desktop />
-						<StatesMap data={data} />
-						<Text
-							weight={500}
-							xs={2}
-							md={3}
-							style={{ margin: '0 1rem -0.5rem' }}
-						>
-							{hoveredTitle}
-						</Text>
-						<StatsCard prop={caseProp} data={hoveredData} />
-						<StatsCard prop={deathProp} data={hoveredData} />
-						<StatsCard prop={recoveredProp} data={hoveredData} />
-					</Grid.Column>
-				</Grid.Row>
-			</Container>
-			<RangeInput dates={dates} totals={totals} />
+			<Switch>
+				<Route path='/'>
+					<Home />
+				</Route>
+			</Switch>
 		</Layout>
-	)
-}
+	</Router>
+)
 
 export default App
