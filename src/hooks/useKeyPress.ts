fac9e7d2011333f0d13d5425fcfc21cbdca1cc27
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-08 04:31:43 -0300

    :bug: fix useKeyPress

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,19 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react'
 
 type VoidReturn = () => void
 
 const useKeyPress = (targetKey: string, fn: VoidReturn) => {
-	const handler = ({ key }: { key: string }) => {
-		if (key === targetKey) return fn()
-	}
+	const handler = useCallback(
+		({ key }: { key: string }) => {
+			if (key === targetKey) return fn()
+		},
+		[targetKey, fn],
+	)
 
 	useEffect(() => {
 		window.addEventListener('keydown', handler)
 		return () => {
 			window.removeEventListener('keydown', handler)
 		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+	}, [handler, targetKey, fn])
 }
 
 export default useKeyPress
