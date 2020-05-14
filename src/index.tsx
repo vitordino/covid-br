f71f773b16f4947f4b99725330615180d56f95dc
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-14 03:54:29 -0300

    :zap: unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import { hydrate, render } from 'react-dom'
 import App from 'App'
-import * as serviceWorker from 'serviceWorker'
+import { unregister } from './serviceWorker'
 
 const ToRender = () => (
 	<StrictMode>
@@ -16,4 +16,4 @@ if (!rootElement?.hasChildNodes()) render(<ToRender />, rootElement)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+unregister()
