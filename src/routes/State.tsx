83f2b10bf5043035da35c34ef92b8dda80d07a93
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-19 01:59:09 -0300

    :sparkles: get each state data

--- src/routes/State.tsx
+++ src/routes/State.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import useSWR from 'swr'
 
-const State = (props: any) => <pre>{JSON.stringify(props, null, 2)}</pre>
+import fetcher from 'utils/fetcher'
+import Container from 'components/Container'
+
+type StateProps = {
+	id: string
+}
+
+const State = ({ id }: StateProps) => {
+	const { data, error } = useSWR<any>(
+		`/data/${id}.json`,
+		fetcher,
+		{ suspense: true },
+	)
+	if(!data || error) return null
+	return (
+		<Container>
+			<pre>{JSON.stringify({ data }, null, 2)}</pre>
+		</Container>
+	)
+}
 
 export default State
