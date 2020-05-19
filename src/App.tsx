5f1d8d32dea897bea0b2214fd5b871b53d7779f1
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-19 01:41:16 -0300

    :sparkles: mock states routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import statesMeta from 'data/statesMeta.json'
 import Layout from 'components/Layout'
 import Loader from 'components/Loader'
 
-const Home = lazy(() => import('./routes/Home')) 
+const Home = lazy(() => import('./routes/Home'))
+const State = lazy(() => import('./routes/State'))
 const NotFound = lazy(() => import('./routes/NotFound'))
 
 const App = () => (
 	<Router>
 		<Layout>
 			<Suspense fallback={<Loader />}>
-			<Switch>
-				<Route path='/' exact>
-					<Home />
-				</Route>
-				<Route path='*'>
-					<NotFound />
-				</Route>
-			</Switch>
+				<Switch>
+					<Route path='/' exact>
+						<Home />
+					</Route>
+					<Route path='/:id' exact>
+						{({ match }) => {
+							const id: string | null = match?.params?.id
+							if (!id || !(id.toUpperCase() in statesMeta)) return <NotFound />
+							return <State id={match?.params?.id} />
+						}}
+					</Route>
+					<Route path='*'>
+						<NotFound />
+					</Route>
+				</Switch>
 			</Suspense>
 		</Layout>
 	</Router>
---
+++ src/routes/State.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const State = (props: any) => <pre>{JSON.stringify(props, null, 2)}</pre>
+
+export default State
