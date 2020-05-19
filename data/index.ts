21862a3cc5e745fd11045c2e257aa7d3f32521d9
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-19 02:27:07 -0300

    :sparkles: await getCountryDate

--- data/index.ts
+++ data/index.ts
@@ -3,8 +3,8 @@ const getStatesData = require('./getStatesData')
 const copyStatesMeta = require('./copyStatesMeta')
 const downloadTopo = require('./downloadTopo')
 
-const main = () => {
-	getCountryData()
+const main = async () => {
+	await getCountryData()
 	getStatesData()
 	downloadTopo()
 }
