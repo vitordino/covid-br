510695dd3792f65235e03ef1a57453319201e7c6
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-22 01:49:17 -0300

    :truck: separate shared table functions

--- src/components/Table/CountryTable.tsx
+++ src/components/Table/CountryTable.tsx
@@ -1,16 +1,24 @@
-import React, { ReactNode, useMemo, useEffect } from 'react'
-import styled, { css } from 'styled-components'
+import React, { useMemo, useEffect } from 'react'
 import { useTable, useSortBy, Column } from 'react-table'
 
-import range from 'utils/range'
 import useStore from 'store'
-import type { Transform } from 'components/Text'
-import Text from 'components/Text'
 
-type RowProps = {
-	values: StateEntry
-	index: number
-}
+import type { RowProps, CellProps } from './shared'
+
+import {
+	Cell,
+	DynamicCell,
+	Header,
+	TotalRow,
+	Wrapper,
+	Table,
+	Mobile,
+	Desktop,
+	TableRow,
+	initialState,
+	getCellRender,
+	EmptyCells,
+} from './shared'
 
 type Cell = {
 	row: RowProps
@@ -18,190 +26,6 @@ type Cell = {
 	data: StateEntry[]
 	[key: string]: any
 }
-
-type Header = any
-
-type HeaderProps = {
-	children?: ReactNode
-	column: {
-		isSorted: boolean
-		isSortedDesc: boolean
-	}
-	[key: string]: any
-	isVisible: boolean
-}
-
-type StaticCellProps = {
-	left?: ReactNode
-	children?: ReactNode
-	bold?: boolean
-	transform?: Transform
-}
-
-const CellWrapper = styled(Text)`
-	padding: 0.375rem 0.75rem;
-	display: flex;
-	position: relative;
-	align-items: baseline;
-	& > * {
-		flex: 1;
-	}
-`
-
-const Left = styled(Text)`
-	display: none;
-	${p => p.theme.above('sm')`
-		display: block;
-	`}
-`
-
-const Cell = ({ left, children, transform, bold = true }: StaticCellProps) => (
-	<CellWrapper transform={transform}>
-		{!!left && <Left>{left}</Left>}
-		{!!left && '\t'}
-		{bold && <strong>{children}</strong>}
-		{!bold && <div>{children}</div>}
-	</CellWrapper>
-)
-
-type DynamicCellProps = {
-	row: RowProps
-	data?: StateEntry[]
-	column: { id: keyof StateEntry }
-	prop?: keyof StateEntry
-	leftProp?: keyof StateEntry
-	leftRender?: (x: ReactNode) => ReactNode
-	mainRender?: (x: ReactNode) => ReactNode
-	children?: ReactNode
-	isVisible: boolean
-}
-
-const DynamicCell = ({
-	row,
-	column,
-	data,
-	prop,
-	leftProp,
-	leftRender = x => `+${x}`,
-	mainRender = x => x,
-	children,
-	isVisible,
-}: DynamicCellProps) => {
-	if (!isVisible) return null
-	return (
-		<Cell left={leftProp && leftRender(data?.[row.index]?.[leftProp])}>
-			{children || mainRender(row.values?.[prop || column.id])}
-		</Cell>
-	)
-}
-
-const HeaderWrapper = styled(Text)`
-	display: flex;
-	justify-content: space-between;
-	padding: 0.5rem 0.75rem;
-	border-radius: 0 0 0.25rem 0.25rem;
-	background: var(--color-base06);
-	color: var(--color-base66);
-	box-shadow: 0 0 0 0.25rem var(--color-base00);
-	margin-bottom: 0.25rem;
-	${p => p.theme.above('md')`
-		border-radius: 0.25rem;
-	`}
-	&:hover {
-		background: var(--color-base);
-		color: var(--color-base00);
-	}
-`
-
-const Header = ({ children, column, isVisible }: HeaderProps) => {
-	if (!isVisible) return null
-	return (
-		<HeaderWrapper>
-			<strong>{children}</strong>{' '}
-			<span>{column.isSorted ? (column.isSortedDesc ? '↓' : '↑') : '·'}</span>
-		</HeaderWrapper>
-	)
-}
-
-const TotalRow = styled.tr`
-	td {
-		background: transparent !important;
-		position: sticky;
-		bottom: 0.5rem;
-		padding-top: 0.5rem;
-	}
-	& > * > * {
-		background: var(--color-base00);
-		padding-top: 0.75rem;
-		padding-bottom: 0.75rem;
-		border-top: 1px solid var(--color-base11);
-		margin-top: 0.125rem;
-		/* box-shadow: 0 -0.5rem 1rem var(--color-base00); */
-	}
-`
-
-const Wrapper = styled.div`
-	padding: 0 0.125rem;
-`
-
-// prettier-ignore
-const Table = styled.table`
-	width: 100%;
-	text-align: right;
-	margin: 0 -0.25rem;
-	position: relative;
-	th:first-child,
-	td:first-child {
-		text-align: left;
-	}
-	th {
-		position: sticky;
-		top: 2.75rem;
-		z-index: 1;
-		${p => p.theme.above('md')`
-			top: 0.25rem;
-		`}
-		&:nth-child(1) { z-index: 8 }
-		&:nth-child(2) { z-index: 7 }
-		&:nth-child(3) { z-index: 6 }
-		&:nth-child(4) { z-index: 5 }
-		&:nth-child(5) { z-index: 4 }
-		&:nth-child(6) { z-index: 3 }
-		&:nth-child(7) { z-index: 2 }
-		&:nth-child(8) { z-index: 1 }
-	}
-	tr:nth-child(2n) > * {
-		background: var(--color-base03);
-	}
-`
-
-const Mobile = styled.span`
-	${p => p.theme.above('md')`
-		display: none;
-	`}
-`
-const Desktop = styled.span`
-	display: none;
-	${p => p.theme.above('md')`
-		display: inline;
-	`}
-`
-
-type TableRowProps = { active: boolean }
-
-const activeStyle = css`
-	& > * {
-		background: yellow !important;
-		color: ${p => p.theme.colors.light.base};
-	}
-`
-
-const TableRow = styled.tr<TableRowProps>`
-	&:hover {
-		${activeStyle}
-	}
-	${p => p.active && activeStyle}
-`
 
 type StateMeta = {
 	p: number
@@ -212,57 +36,6 @@ type CountryTableProps = {
 	data: StateEntry[]
 	total: StateEntry
 	statesMeta: StatesMeta
-}
-
-type SortByOptions = {
-	id: keyof StateEntry
-	desc: boolean
-}
-
-type InitialTableState = {
-	sortBy: SortByOptions[]
-}
-
-const initialState: InitialTableState = {
-	sortBy: [{ id: 'tc', desc: true }],
-}
-
-const RelativeRender = ({ x, isNew }: { x: number; isNew?: boolean }) => (
-	<span title={`${isNew ? '+ ' : ''}${x * 10000} a cada 10 mil hab`}>
-		{!!isNew && x > 0 && '+ '}
-		{(x * 10000).toFixed(2)}‱
-	</span>
-)
-
-const AbsoluteRender = ({ x, isNew }: { x: number; isNew?: boolean }) => (
-	<span>
-		{!!isNew && x > 0 && '+ '}
-		{x}
-	</span>
-)
-
-const getCellRender = (relative: boolean, isNew?: boolean) => (
-	x: ReactNode,
-) => {
-	if (typeof x !== 'number') return x
-	if (relative) return <RelativeRender x={x} isNew={isNew} />
-	return <AbsoluteRender x={x} isNew={isNew} />
-}
-
-type EmptyCellsProps = {
-	count: number
-	isVisible?: boolean
-}
-
-const EmptyCells = ({ count, isVisible = true }: EmptyCellsProps) => {
-	if (!isVisible) return null
-	return (
-		<>
-			{range(count).map(x => (
-				<td key={x} />
-			))}
-		</>
-	)
 }
 
 const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
@@ -285,14 +58,14 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 		() => [
 			{
 				accessor: 'st',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={true} {...x}>
 						<Mobile>St</Mobile>
 						<Desktop>State</Desktop>
 					</Header>
 				),
 				sortInverted: true,
-				Cell: ({ row }: Cell) => (
+				Cell: ({ row }: CellProps) => (
 					<Cell>
 						<span
 							// @ts-ignore
@@ -305,7 +78,7 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 			},
 			{
 				accessor: 'tc',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={!relative} {...x}>
 						Confirmed
 					</Header>
@@ -325,7 +98,7 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 			},
 			{
 				accessor: 'td',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={!relative} {...x}>
 						Deaths
 					</Header>
@@ -345,7 +118,7 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 			},
 			{
 				accessor: 'tr',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={!relative} {...x}>
 						Recovered
 					</Header>
@@ -366,7 +139,7 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 			{
 				accessor: 'ptc',
 				sortType: 'basic',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={relative} {...x}>
 						Confirmed
 					</Header>
@@ -387,7 +160,7 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 			{
 				accessor: 'ptd',
 				sortType: 'basic',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={relative} {...x}>
 						Deaths
 					</Header>
@@ -408,7 +181,7 @@ const CountryTable = ({ data, total, statesMeta }: CountryTableProps) => {
 			{
 				accessor: 'ptr',
 				sortType: 'basic',
-				Header: (x: Header) => (
+				Header: (x: any) => (
 					<Header isVisible={relative} {...x}>
 						Recovered
 					</Header>
