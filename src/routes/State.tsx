5fecac4bf319baa92ab7a1d6db50a7c8f31c9183
Vitor Dino <vitor@kunstdesign.com.br>
2020-05-22 03:04:45 -0300

    :sparkles: show current date state data

--- src/routes/State.tsx
+++ src/routes/State.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useMemo, useLayoutEffect } from 'react'
 import useSWR from 'swr'
 
+import statesMeta from 'data/statesMeta.json'
+import useStore from 'store'
+import dateToString from 'utils/dateToString'
 import fetcher from 'utils/fetcher'
 import Container from 'components/Container'
+import TitleHeader from 'components/TitleHeader'
+import RangeInput from 'components/RangeInput'
 
 type StateProps = {
-	id: string
+	id: keyof typeof StatesEnum
+}
+
+const getStateName = (id: keyof typeof StatesEnum) => statesMeta?.[id]?.n
+
+type StateDataType = {
+	main: Main
+	totals: Totals
+	dates: DatesEnum[]
+}
+
+interface InnerProps extends StateDataType {
+	id: keyof typeof StatesEnum
+}
+
+const Inner = ({ id, main, totals, dates }: InnerProps) => {
+	const [dateIndex, setDateIndex] = useStore(s => [s.dateIndex, s.setDateIndex])
+	/* eslint-disable react-hooks/exhaustive-deps */
+	const total = useMemo(() => totals[dates[dateIndex]], [dateIndex])
+	const data = useMemo(() => main[dates[dateIndex]], [dateIndex])
+	/* eslint-enable react-hooks/exhaustive-deps */
+
+	useLayoutEffect(() => {
+		setDateIndex(dates.length - 1)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	return (
+		<>
+			<Container>
+				<TitleHeader
+					title={getStateName(id)}
+					options={dates}
+					value={dateIndex.toString()}
+					onChange={v => setDateIndex(parseInt(v))}
+					renderOption={dateToString}
+				/>
+
+				<pre>{JSON.stringify({ data, total }, null, 2)}</pre>
+			</Container>
+			<RangeInput dates={dates} totals={totals} />
+		</>
+	)
 }
 
 const State = ({ id }: StateProps) => {
@@ -13,11 +60,8 @@ const State = ({ id }: StateProps) => {
 		suspense: true,
 	})
 	if (!data || error) return null
-	return (
-		<Container>
-			<pre>{JSON.stringify({ data }, null, 2)}</pre>
-		</Container>
-	)
+	const { main, dates, totals } = data
+	return <Inner id={id} main={main} dates={dates} totals={totals} />
 }
 
 export default State
