0e70177d9807b43315f003bd05bb9fe2d9a48fd0
Vitor Dino <vitor@kunstdesign.com.br>
2020-06-06 21:33:26 -0300

    :lipstick: 404

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -2,19 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Container from 'components/Container'
+import SEO from 'components/SEO'
+import Spacer from 'components/Spacer'
 import Text from 'components/Text'
 
 const Wrapper = styled(Container)`
 	display: flex;
-	align-items: center;
+	flex-direction: column;
 	justify-content: center;
-	padding: 10rem 0;
+	padding-top: 10rem;
+	padding-bottom: 10rem;
 `
 
 const NotFound = () => (
 	<Wrapper>
+		<SEO title='404' />
 		<Text xs={6} weight={600}>
 			404
+		</Text>
+		<Spacer.V xs={1} />
+		<Text xs={4} weight={500}>
+			Página não encontrada
 		</Text>
 	</Wrapper>
 )
